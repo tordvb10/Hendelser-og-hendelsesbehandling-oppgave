13cf791fbc42b0cd24f4f2a2aa26eec74e526800
JoakimJobloop <Joakim@jobloop.no>
2024-02-13 11:03:35 +0100

    updated assignment numbering

--- js/soundboard_oppgave.js
+++ js/soundboard_oppgave.js
@@ -10,54 +10,54 @@
 
 //* ////////////////////////////////////// */
 
-//*1. Catch the html element with id drumkit: */
+//*1.1. Catch the html element with id drumkit: */
 
 
 //* ////////////////////////////////////// */
 
-//*2. Write a console log for the fetched sounds so you know how the structure is and how you can use it */
+//*1.3. Write a console log for the fetched sounds so you know how the structure is and how you can use it */
 
 
 //* ////////////////////////////////////// */
 
 
-//*3. Create the function with a parameter that is refering to the sound used that does the following: */
+//*2. Create the function with a parameter that is refering to the sound used that does the following: */
 
-//3.1. make a variable that creates a button element with .createElement
+//2.1. make a variable that creates a button element with .createElement
 
     // add textContent to the created buttonElement. Textcontent should be either the file name and/or key needed to be pressed
 
-//3.2. make a variables that create an audio element with .createElement 
+//2.2. make a variables that create an audio element with .createElement 
     //the audio element that is created should have the src equal to the file source
-    //the audio element that is created should have the id equal to the textcontent created in 3.1.
+    //the audio element that is created should have the id equal to the textcontent created in 2.1.
 
-//3.3. add an eventlistner to the whole page that: 
+//2.3. add an eventlistner to the whole page that: 
     //actives when pressing a keyboard key (first parameter of the eventlistener)
     //runs a nameless function with parameter event (refering to the key pressed)
 
-    // 3.3.1. inside of the eventlistner:
+    // 2.3.1. inside of the eventlistner:
 
         // create a variable that refers to the key pressed 
         // make a conditional logic that asks if the variable created just above is the same as the sound key that should be pressed (the key "key" in the js or JSON data you created)
-            // if the conditional is true, play the audio element that you created in 3.2.
+            // if the conditional is true, play the audio element that you created in 2.2.
         
-//3.4. OPTIONAL. If you used keydown as the first parameter in the previous eventlistener, add another eventlistner to the whole page that: 
+//2.4. OPTIONAL. If you used keydown as the first parameter in the previous eventlistener, add another eventlistner to the whole page that: 
     //actives when releasing a keyboard key (first parameter of the eventlistener)
 
-    // 3.4.1. inside of the eventlistner:
+    // 2.4.1. inside of the eventlistner:
 
         // pauses the audio element
         // sets the current time of the audio element to 0
 
-//3.5. OPTIONAL. Create an eventlistener for clicking. Also create a logic for preventing more sounds to be played at the same time
+//2.5. OPTIONAL. Create an eventlistener for clicking. Also create a logic for preventing more sounds to be played at the same time
 
-//3.6. append the created button and audio element to the html element you refered in 1.
+//2.6. append the created button and audio element to the html element you refered in 1.
 
 
 //* ///////////////////////////////////////////////////////////////////////////////////////////// */
 
 
-//*4. Create a function that loops over the sounds (from the data file you created). Use that function created in 3. to use the logic there to create the buttons. I prefer that you use .forEach or .map */
+//*3. Create a function that loops over the sounds (from the data file you created). Use that function created in 2. to use the logic there to create the buttons. I prefer that you use .forEach or .map */
 
 
 //* ////////////////////////////////////// */
